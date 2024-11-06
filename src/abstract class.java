1a3812639ddca3575538427f517704833368f20e
Steve5829 <chen.12215@buckeyemail.osu.edu>
2024-11-06 04:47:58 +0000

    resub

--- src/abstract class.java
+++ src/abstract class.java
@@ -1,6 +1,5 @@
 import components.map.Map;
-import components.map.Map1L;
-
+import components.map.Map.Pair;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -9,42 +8,26 @@ import java.util.logging.Logger;
 import java.util.logging.Level;
 
 public abstract class TrackerSecondary implements Tracker {
-    //logging to replace outprint
     private static final Logger logger = Logger.getLogger(TrackerSecondary.class.getName());
 
-    protected Map<String, Double> trackMap;
-
-    public TrackerSecondary() {
-        this.trackMap = new Map1L<>();
-        logger.log(Level.INFO, "TrackerSecondary instance created.");
-    }
-
-    // Secondary method
     public void sortTrack() {
         logger.log(Level.INFO, "Sorting trackMap by amount in ascending order...");
-
-        // Extract entries from trackMap into a list
-        List<Map.Pair<String, Double>> entries = new ArrayList<>();
-        for (Map.Pair<String, Double> pair : this.trackMap) {
+        List<Pair<String, Double>> entries = new ArrayList<>();
+        for (Pair<String, Double> pair : this.getMap()) {
             entries.add(pair);
         }
-
-        // Sort the list of entries by amount in ascending order
-        Collections.sort(entries, Comparator.comparingDouble(Map.Pair::value));
-
-        // Clear trackMap and put sorted entries back
-        this.trackMap.clear();
-        for (Map.Pair<String, Double> pair : entries) {
-            this.trackMap.add(pair.key(), pair.value());
+        Collections.sort(entries, Comparator.comparingDouble(Pair::value));
+        this.clearMap();
+        for (Pair<String, Double> pair : entries) {
+            this.addMapEntry(pair.key(), pair.value());
         }
-
         logger.log(Level.INFO, "trackMap sorted by amount.");
     }
 
     @Override
     public String toString() {
-        String result = "";
-        for (Map.Pair<String, Double> pair : this.trackMap) {
+        StringBuilder result = new StringBuilder();
+        for (Pair<String, Double> pair : this.getMap()) {
             result.append("Type: ").append(pair.key())
                   .append(", Amount: ").append(pair.value()).append("\n");
         }
@@ -54,23 +37,26 @@ public abstract class TrackerSecondary implements Tracker {
 
     @Override
     public boolean equals(Object obj) {
-        String result = "";
         if (this == obj) {
-        result = true;
+            return true;
         }
         if (!(obj instanceof TrackerSecondary)) {
-            result = false;
+            return false;
         }
         TrackerSecondary other = (TrackerSecondary) obj;
-        boolean isEqual = this.trackMap.equals(other.trackMap);
+        boolean isEqual = this.getMap().equals(other.getMap());
         logger.log(Level.FINE, "Comparing trackMap for equality: {0}", isEqual);
-        return result;
+        return isEqual;
     }
 
     @Override
     public int hashCode() {
-        int hash = this.trackMap.hashCode();
+        int hash = this.getMap().hashCode();
         logger.log(Level.FINE, "Computed hashCode for trackMap: {0}", hash);
         return hash;
     }
+
+    protected abstract Map<String, Double> getMap();
+    protected abstract void clearMap();
+    protected abstract void addMapEntry(String key, Double value);
 }
